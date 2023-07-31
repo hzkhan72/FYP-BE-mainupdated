08ff815b8647ab7c4c523c6a47f9d4947efeac95
Huzaifa Khan <hzkhan72@gmail.com>
2023-08-01 03:56:45 +0500

    Updated

---
+++ app.py
@@ -0,0 +1,108 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
+
+from model import nlp_model
+from flask import Response
+
+app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+
+@app.before_request
+def basic_authentication():
+    if request.method.lower() == "options":
+        return Response()
+
+
+@app.route("/api/", methods=["GET"], strict_slashes=False)
+@cross_origin(supports_credentials=True)
+def respond():
+    # Retrieve the video_id from url parameter
+    vid_url = request.args.get("video_url", None)
+
+    if "youtube.com" in vid_url:
+        try:
+            video_id = vid_url.split("=")[1]
+
+            try:
+                video_id = video_id.split("&")[0]
+
+            except:
+                video_id = "False"
+
+        except:
+            video_id = "False"
+
+    elif "youtu.be" in vid_url:
+        try:
+            video_id = vid_url.split("/")[3]
+
+        except:
+            video_id = "False"
+
+    else:
+        video_id = "False"
+
+    # For debugging
+    # print(f"got name {video_id}")
+
+    body = {}
+    data = {}
+
+    # Check if user doesn't provided  at all
+    if not video_id:
+        data["message"] = "Failed"
+        data["error"] = "no video id found, please provide valid video id."
+
+    # Check if the user entered a invalid instead video_id
+    elif str(video_id) == "False":
+        data["message"] = "Failed"
+        data["error"] = "video id invalid, please provide valid video id."
+
+    # Now the user has given a valid video id
+    else:
+        if nlp_model(video_id) == "0":
+            data["message"] = "Failed"
+            data["error"] = "API's not able to retrieve Video Transcript."
+
+        else:
+            data["message"] = "Success"
+            data["id"] = video_id
+            (
+                data["topic_name"],
+                data["eng_summary"],
+                data["Urdu_summary"],
+                data["original_txt_length"],
+                data["final_summ_length"],
+            ) = nlp_model(video_id)
+
+    body["data"] = data
+
+    # Return the response in json format
+    return buildResponse(body)
+
+
+# Welcome message to our server
+@app.route("/")
+def index():
+    body = {}
+    body["message"] = "Success"
+    body["data"] = "Welcome to YTS API."
+
+    return buildResponse(body)
+
+
+def buildResponse(body):
+    # from flask import json, Response
+    # res = Response(response=json.dumps(body), status=statusCode, mimetype="application/json")
+    # res.headers["Content-Type"] = "application/json; charset=utf-8"
+    # return res
+
+    response = jsonify(body)
+    # response.headers.add('Access-Control-Allow-Origin', '*')
+    # response.headers.add('Access-Control-Allow-Origin', 'http://localhost:3000')
+    return response
+
+
+if __name__ == "__main__":
+    app.run(threaded=True, debug=True)
